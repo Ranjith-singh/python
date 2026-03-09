5725dcced8a14f945d9ad81f132ba6daab7f8bc2
Ranjith-singh <singhranjith970@gmail.com>
2026-03-09 23:23:35 +0530

    Understanding Deeper about python Basics

--- python_learning/BE_Student.py
+++ python_learning/BE_Student.py
@@ -5,6 +5,15 @@ class BE_Student(Student):
         super().__init__(name, age)
     def cgpa(self):
         return "8.4"
-        
+
 student= BE_Student("john", 13)
 print(student.cgpa())
+
+# li= [2, 2, 3, 5, 5]
+# map= {}
+
+# for i in li:
+#     map[i]= map[i]+1
+# for i in map.keys():
+#     if map[i]>1:
+#         print(i)
